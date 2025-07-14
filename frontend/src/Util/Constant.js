af775272705d3b8569e7929698bc696d85e647fa
rahulrp17 <rahulrp4021@gmail.com>
2025-07-14 11:55:31 +0530

    Update Constant.js

--- frontend/src/Util/Constant.js
+++ frontend/src/Util/Constant.js
@@ -1,3 +1,3 @@
 export const AppConstants = {
-BACKEND_URL: "https://authentication-system-backend-8zx7.onrender.com/api/v1.0:http://localhost:8080/api/v1.0",
+BACKEND_URL: "https://authentication-system-backend-8zx7.onrender.com/api/v1.0",
 }
